7392978ef6d2d8a1da5df6d438c300e878c5b236
Huzaifa <huzaifayamin11@gmail.com>
2019-03-29 17:42:07 +1100

    final

--- greenHouse_bluetooth.py
+++ greenHouse_bluetooth.py
@@ -7,34 +7,6 @@ import monitorAndNotify as util
 
 from sense_hat import SenseHat
 # apiKey = "o.UPscOoahFXH2SOglsP5MICsJSywOOlqr"
-# p = PushBullet(apiKey)
-# # Get a list of devices
-# devices = p.getDevices()
-
-# # Get a list of contacts
-# contacts = p.getContacts()
-
-# # Send a note
-# p.pushNote(devices[0]["iden"], 'Hello world', 'Test body')
-
-# # Send a map location
-# p.pushAddress(devices[0]["iden"], "Eiffel tower", "Eeiffel tower, france")
-
-# # Send a list
-# p.pushList(devices[0]["iden"], "Groceries", ["Apples", "Bread", "Milk"])
-
-# # Send a link
-# p.pushLink(devices[0]["iden"], "Google", "http://www.google.com")
-
-# # Send a file
-# p.pushFile(devices[0]["iden"], "file.txt", "This is a text file", open("file.txt", "rb"))
-
-# # Send a note to a channel
-# p.pushNote('channel_tag', 'Hello world', 'Test body', recipient_type='channel_tag')
-
-# # Send a note to an email
-# p.pushNote('myemail@domain.com', 'Hello world', 'Test body', recipient_type='email')
-
 
 
 class Blconnect():
