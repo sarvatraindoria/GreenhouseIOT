c809011e0dfde147c28511b5e8dbc4a9cd145f4b
Huzi1 <38772193+Huzi1@users.noreply.github.com>
2019-04-04 17:53:11 +1100

    api-key edit

--- greenHouse_bluetooth.py
+++ greenHouse_bluetooth.py
@@ -5,7 +5,7 @@ from pushbullet.pushbullet import PushBullet
 import monitorAndNotify as util
 
 from sense_hat import SenseHat
-# apiKey = "o.UPscOoahFXH2SOglsP5MICsJSywOOlqr"
+# apiKey = "o.2uIo9r0MKCCFX8Mv3uMhp4xOXE9vgBrCr"
 
 # Bluetooth class with scanning and SendMsg funstion
 
@@ -43,7 +43,7 @@ class Blconnect():
 
     def connect(self):
        
-        apiKey = "o.UPscOoahFXH2SOglsP5MICsJSywOOlqr"
+        apiKey = "o.2uIo9r0MKCCFX8Mv3uMhp4xOXE9vgBrCr"
         p = PushBullet(apiKey)
         # Get a list of devices
         devices = p.getDevices()
