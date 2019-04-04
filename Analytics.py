bf2514888f85f8cd11ff525683110bbb2310c71a
Huzaifa <huzaifayamin11@gmail.com>
2019-04-04 15:51:36 +1100

    Analytics raw

---
+++ Analytics.py
@@ -0,0 +1,25 @@
+from importFix import fixImport
+import os
+import sqlite3
+import seaborn as sns
+import pandas as pd
+import pygal
+import cairosvg
+import monitorAndNotify
+
+plot = fixImport().getPLT()
+
+
+class CreateAnalyticsBar():
+    def generateBar():
+            conn = monitorAndNotify.Dbcon().createCon()[1]
+
+            data = pd.read_sql_query('''SELECT date, MIN(temp), MAX(temp), MIN(humid),
+                MAX(humid) FROM pidata GROUP BY date''', conn)
+            conn.close()
+            data = pd.melt(data, id_vars="date", var_name="Attributes", value_name="Value")
+
+            sns.catplot(x='date', y='Value', hue='Attributes', data=data, kind='bar')
+
+            plot.savefig('bar.png')
+CreateAnalyticsBar().generateBar()
