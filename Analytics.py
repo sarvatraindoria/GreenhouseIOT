b38c01566460995fe0096f6311eef6d85334ba9e
Huzaifa <huzaifayamin11@gmail.com>
2019-04-04 16:04:57 +1100

    import fix

--- Analytics.py
+++ Analytics.py
@@ -7,19 +7,33 @@ import pygal
 import cairosvg
 import monitorAndNotify
 
+# Import fix for matplotlib
 plot = fixImport().getPLT()
+
+# Bar graph generator Class with a single function
 
 
 class CreateAnalyticsBar():
     def generateBar():
-            conn = monitorAndNotify.Dbcon().createCon()[1]
+        conn = monitorAndNotify.Dbcon().createCon()[1]
 
-            data = pd.read_sql_query('''SELECT date, MIN(temp), MAX(temp), MIN(humid),
+        data = pd.read_sql_query(
+            '''SELECT date, MIN(temp), MAX(temp), MIN(humid),
                 MAX(humid) FROM pidata GROUP BY date''', conn)
-            conn.close()
-            data = pd.melt(data, id_vars="date", var_name="Attributes", value_name="Value")
+        conn.close()
+        data = pd.melt(
+            data,
+            id_vars="date",
+            var_name="Attributes",
+            value_name="Value")
 
-            sns.catplot(x='date', y='Value', hue='Attributes', data=data, kind='bar')
+        sns.catplot(
+            x='date',
+            y='Value',
+            hue='Attributes',
+            data=data,
+            kind='bar')
 
-            plot.savefig('bar.png')
+        plot.savefig('bar.png')
+
 CreateAnalyticsBar().generateBar()
